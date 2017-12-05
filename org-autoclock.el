53cb1ed4b0dbd731d0439d06819e914c13301013
S Pradeep Kumar <gohanpra@gmail.com>
2017-12-05 03:25:24 -0500

    autoclock: require org; create a clock in log file on startup.
    Extract timer delay variable.

--- org-autoclock.el
+++ org-autoclock.el
@@ -1,4 +1,13 @@
+;;; -*- lexical-binding: t; -*-
 ;;; org-autoclock.el -- Automatically clock in for certain files.
+
+(require 'org)
+
+(defvar org-autoclock-logfile "~/.org-autoclock-log.org"
+  "Log file where the session durations are stored.")
+
+(defvar org-autoclock-timer-delay 5
+  "Delay in seconds before timer automatically clocks in or out.")
 
 (defvar org-autoclock-timer nil "Timer for `org-autoclock`.")
 
@@ -10,19 +19,23 @@ If none exists, signal the user."
   (cond ((org-autoclock-doing-taskp)
 	 (when (equal (org-clock-in-last) "No last clock")
 	   (user-error "Please clock in to an Org task before using org-autoclock.")))
-	;; No pesky clocking messages.
-	(t (let* ((inhibit-message t))
-	     (org-clock-out nil t)))))
+	(t (org-clock-out nil t))))
 
 (defun org-autoclock-doing-taskp (&optional arg)
   "Return true if we are doing the specified task. (For now, C code.)"
   (interactive "P")
-  (and (buffer-file-name) (s-match "[cSh]" (f-ext (buffer-file-name)))))
+  (and (buffer-file-name) (string-match-p "\\.[cSh]$" (buffer-file-name))))
 
 (defun org-autoclock-start (&optional arg)
   "Start automatically clocking into a desired task."
   (interactive "P")
-  (setq org-autoclock-timer (run-with-timer 1 5 'org-autoclock)))
+  (with-current-buffer (find-file-noselect org-autoclock-logfile)
+    (goto-char (point-max))
+    (insert "\n* XINU task")
+    (org-clock-in)
+    (org-clock-out)
+    (save-buffer))
+  (setq org-autoclock-timer (run-with-timer 1 org-autoclock-timer-delay 'org-autoclock)))
 
 (defun org-autoclock-stop (&optional arg)
   "Cancel automatic clocking. Clock out of any running clock."
